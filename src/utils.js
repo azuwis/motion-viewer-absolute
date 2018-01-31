ab03bd09433b63170029faa77e97e576617c4714
Zhong Jianxin <azuwis@gmail.com>
2018-01-31 13:36:29 +0800

    Fix getDate by padding month and date

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,7 @@
+function padDate (str) {
+  return String('00' + str).slice(-2)
+}
+
 export function getDate (date) {
-  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+  return `${date.getFullYear()}-${padDate(date.getMonth() + 1)}-${padDate(date.getDate())}`
 }
